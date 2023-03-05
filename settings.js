9df989b86cede7a937c65584fcd1037eed72b29a
thejsry0900 <127011371+thejsry0900@users.noreply.github.com>
2023-03-05 19:59:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,15 +16,15 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
-global.owner = ['94774071805'] //ur owner number
-global.ownername = "Kaveesha Sithum" //ur owner name
+global.owner = ['8281358607'] //ur owner number
+global.ownername = "SRY" //ur owner name
 global.ytname = "YT: kavee" //ur yt chanel name
 global.socialm = "Dark Nero" //ur github or insta name
 global.location = "Sri Lanka, Gampaha, Kurunegala" //ur location
 
 //bot bomdy 
-global.ownernomer = "94774071805" //ur number
-global.premium = ['94774071805'] //ur premium number
+global.ownernomer = "8281358607" //ur number
+global.premium = ['8281358607'] //ur premium number
 global.botname = 'DarkNero Bot MD V7' //ur bot name
 global.linkz = "https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX" //your theme url which will be displayed on whatsapp
 global.websitex = "https://www.youtube.com/@kaveebroyt5303" //ur website to be displayed
